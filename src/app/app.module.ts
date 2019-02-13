76bd00b1deed8242e62efa221e47952148d0c1f7
Grigor Sargsyan <sargr86@gmail.com>
2019-02-13 08:56:56 +0400

    Integrated resize with ngResizable option of ngx-draggable package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AngularDraggableModule} from 'angular2-draggable';
 
-import { AppComponent } from './app.component';
-import { RectangleComponent } from './rectangle/rectangle.component';
+import {AppComponent} from './app.component';
+import {RectangleComponent} from './components/rectangle/rectangle.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RectangleComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        RectangleComponent
+    ],
+    imports: [
+        BrowserModule,
+        AngularDraggableModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
